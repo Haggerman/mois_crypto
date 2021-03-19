b83c942060e6ff35c64d47525768ce9c7b30654c
Haggerman <jan.ther@seznam.cz>
2021-03-19 13:56:02 +0100

    Jen odstraneno par radku

--- src/views/data_fetch/SearchableList.js
+++ src/views/data_fetch/SearchableList.js
@@ -5,42 +5,7 @@ import { useState, useEffect } from 'react';
 import useFetch from './useFetch'
 
 const SearchableList = () => {
-  const columns = 
-     [
-      {
-        label: 'Name',
-        field: 'name',
-        sort: 'asc',
-        width: 150
-      },
-      {
-        label: 'Symbol',
-        field: 'symbol',
-        sort: 'asc',
-        width: 270
-      },
-      {
-        label: 'Price change 24h',
-        field: 'price_change_24h',
-        sort: 'asc',
-        width: 200
-      },
-      {
-        label: 'ATH',
-        field: 'ath',
-        sort: 'asc',
-        width: 100
-      },
-      {
-        label: 'Price',
-        field: 'current_price',
-        sort: 'asc',
-        width: 150
-      },
-    ];
-
 const { data, error, isPending} = useFetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=&page=1&sparkline=false&price_change_percentage=24h');
-
 const [dataTable, setData] = useState({});
 useEffect(() => {
   if(data){
